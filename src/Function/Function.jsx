638f5f69a9bfaa71d7ebe897691c39f71000eb4c
Eddy Chen <eddychen615@gmail.com>
2023-11-19 04:03:15 +0900

    function part adjust.

--- src/Function/Function.jsx
+++ src/Function/Function.jsx
@@ -4,65 +4,24 @@ import React from 'react';
 import '@/Function/style/Function.css';
 import '@/app/globals.css';
 
-const hexagon = [
-  {
-    hexagonclass: 'hexagon01',
-    centerclass01: 'center01',
-    centerclass03: 'center02',
-    centerclass02: 'center03',
-    title: '社内報管理',
-    text: '',
-  },
-  {
-    hexagonclass: 'hexagon02',
-    title: '社内報作成',
-    text: '',
-  },
-  {
-    hexagonclass: 'hexagon03',
-    title: 'セキュリティ',
-    text: '',
-  },
-  {
-    hexagonclass: 'hexagon04',
-    title: '閲覧',
-    text: '',
-  },
-  {
-    hexagonclass: 'hexagon05',
-    title: '分析',
-    text: '',
-  },
-  {
-    hexagonclass: 'hexagon06',
-    title: 'その他',
-    text: '',
-  },
-  {
-    hexagonclass: 'hexagon07',
-    title: '',
-    text: '',
-  },
-];
-
 export default function Function() {
   return (
     <div>
       <div className="bg-white h-screen relative">
         <h3 className="text-[32px] font-bold text-center pt-16">機能一覧</h3>
 
-        <div className="hexagon flex flex-col relative top-[10%]">
+        <div className="hexagon flex flex-col relative top-[8%]">
           <div className="flex justify-center w-full h-full">
             <div className="hexagon01 h-[12%] w-[12%] relative">
-              <img src="/img/hexagon-O.svg" />
+              <img src="/img/hexagon-O.svg" className="duration-500" />
               <h4
-                className="text-h4 text-center font-semibold text-[#e5836c]
+                className="text-h4 text-center font-semibold text-[#e5836c] duration-500
               absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full "
               >
                 社内報管理
               </h4>
               <ul
-                className="text-text text-center text-[#e5836c]
+                className="text-text text-center text-[#e5836c] duration-500
               absolute top-0 leading-[40px] left-1/2 w-[200%] translate-x-[-50%]
               opacity-0"
               >
@@ -79,15 +38,15 @@ export default function Function() {
               </ul>
             </div>
             <div className="hexagon02 h-[12%] w-[12%] relative">
-              <img src="/img/hexagon-B.svg" />
+              <img src="/img/hexagon-B.svg" className="duration-500" />
               <h4
-                className="text-h4 text-center font-semibold text-[#318ebf]
+                className="text-h4 text-center font-semibold text-[#318ebf] duration-500
               absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full "
               >
                 社内報作成
               </h4>
               <ul
-                className="text-text text-center text-[#318ebf]
+                className="text-text text-center text-[#318ebf] duration-500
               absolute top-0 leading-[40px] left-1/2 w-[150%] translate-x-[-50%]
               opacity-0"
               >
@@ -108,15 +67,88 @@ export default function Function() {
           </div>
           <div className="flex justify-center mt-[-30px]">
             <div className="hexagon03 h-[12%] w-[12%] relative">
-              <img src="/img/hexagon-T.svg" />
+              <img src="/img/hexagon-P.svg" className="duration-500" />
               <h4
-                className="text-h4 text-center font-semibold text-[#a08b90]
+                className="text-h4 text-center font-semibold text-[#a3619d] duration-500
+              absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full "
+              >
+                分析
+              </h4>
+              <ul
+                className="text-text text-center text-[#a3619d] duration-500
+              absolute top-0 leading-[40px] left-1/2 w-[150%] translate-x-[-50%]
+              opacity-0"
+              >
+                <li className="font-semibold text-text">
+                  わかりやすいダッシュボード
+                </li>
+                <li className="font-semibold text-text">
+                  結果が簡単にわかる分析サマリ
+                </li>
+                <li className="font-semibold text-text">
+                  読了率までわかる詳細な分析
+                </li>
+                <li className="font-semibold text-text">
+                  記事ごとの分析数値一覧
+                </li>
+                <li className="font-semibold text-text">
+                  指標別に人気/不人気記事を分析
+                </li>
+                <li className="font-semibold text-text">記事ごとの分析機能</li>
+                <li className="font-semibold text-text">
+                  自由な分析項目カスタマイズ
+                </li>
+                <li className="font-semibold text-text">
+                  閲覧者の属性・グループごとの分析
+                </li>
+                <li className="font-semibold text-text">
+                  変化のわかる時系列データ
+                </li>
+              </ul>
+            </div>
+
+            <div className="hexagon04 h-[12%] w-[12%] relative">
+              <img src="/img/hexagon-R.svg" />
+              <img
+                src="/img/ourly_logo.webp"
+                alt="ourly logo"
+                className="absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-[80%]"
+              />
+            </div>
+            <div className="hexagon05 h-[12%] w-[12%] relative">
+              <img src="/img/hexagon-G.svg" className="duration-500" />
+              <h4
+                className="text-h4 text-center font-semibold text-[#21aba2] duration-500
+              absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full "
+              >
+                セキュリティ
+              </h4>
+              <ul
+                className="text-text text-center text-[#21aba2] duration-500
+              absolute top-0 leading-[40px] left-1/2 w-[150%] translate-x-[-50%]
+              opacity-0"
+              >
+                <li className="font-semibold text-text">
+                  国際規格に準拠した情報管理体制
+                </li>
+                <li className="font-semibold text-text">安心のセキュリティ</li>
+                <li className="font-semibold text-text">
+                  SAML認証によるSSO（シングルサインオン）に対応
+                </li>
+              </ul>
+            </div>
+          </div>
+          <div className="flex justify-center mt-[-30px]">
+            <div className="hexagon06 h-[12%] w-[12%] relative">
+              <img src="/img/hexagon-T.svg" className="duration-500" />
+              <h4
+                className="text-h4 text-center font-semibold text-[#a08b90] duration-500
               absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full "
               >
                 その他
               </h4>
               <ul
-                className="text-text text-center text-[#a08b90]
+                className="text-text text-center text-[#a08b90] duration-500
               absolute top-0 leading-[40px] left-1/2 w-[150%] translate-x-[-50%]
               opacity-0"
               >
@@ -128,19 +160,29 @@ export default function Function() {
                 </li>
               </ul>
             </div>
-            <div className="h-[12%] w-[12%]">
-              <img src="/img/hexagon-R.svg" />
-            </div>
-            <div className="h-[12%] w-[12%]">
-              <img src="/img/hexagon-G.svg" />
-            </div>
-          </div>
-          <div className="flex justify-center mt-[-30px]">
-            <div className="h-[12%] w-[12%]">
-              <img src="/img/hexagon-P.svg" />
-            </div>
-            <div className="h-[12%] w-[12%]">
-              <img src="/img/hexagon-Y.svg" />
+            <div className="hexagon07 h-[12%] w-[12%] relative">
+              <img src="/img/hexagon-Y.svg" className="duration-500" />
+              <h4
+                className="text-h4 text-center font-semibold text-[#f99106] duration-500
+              absolute top-1/2 left-1/2 translate-x-[-50%] translate-y-[-50%] w-full "
+              >
+                閲覧
+              </h4>
+              <ul
+                className="text-text text-center text-[#f99106] duration-500
+              absolute top-0 leading-[40px] left-1/2 w-[150%] translate-x-[-50%]
+              opacity-0"
+              >
+                <li className="font-semibold text-text">
+                  いいね・コメント機能
+                </li>
+                <li className="font-semibold text-text">記事検索機能</li>
+                <li className="font-semibold text-text">マルチデバイス対応</li>
+                <li className="font-semibold text-text">通知機能</li>
+                <li className="font-semibold text-text">
+                  アプリとしてインストール可能
+                </li>
+              </ul>
             </div>
           </div>
         </div>
