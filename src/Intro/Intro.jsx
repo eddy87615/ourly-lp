32579d910da2fbffdfb4627668fa83b890af49c0
Eddy Chen <eddychen615@gmail.com>
2023-11-24 09:54:08 +0900

    500 and 768 position change(insert order) and intro done.

--- src/Intro/Intro.jsx
+++ src/Intro/Intro.jsx
@@ -11,35 +11,45 @@ export default function Intro() {
       id="section0"
     >
       <p
-        className="text-h3 lg:text-h2
-      font-bold p-4 lg:pr-[10%] top-[10%] absolute MD:static lg:static "
+        className="text-h3 lg:text-h2 text-center lg:text-left
+      font-bold px-24 lg:p-4 
+      lg:pr-[10%] top-[10%] md:top-[16%] lg:top-[10%] md:left-1/2 md:translate-x-[-50%] md:w-full
+      absolute MD:static lg:static "
       >
         <span
-          className=" text-[--ourly-theme] absolute opacity-50 top-[10%] right-[30%] 
-        text-h3 hidden sm:block lg:block
+          className=" text-[--ourly-theme] absolute opacity-50 
+          lg:top-[10%] lg:right-[30%] 
+        text-h3 
+        hidden md:hidden lg:block
         font-semibold"
         >
           会社の雰囲気を改善したい…
         </span>
         <span
-          className=" text-[--ourly-theme] absolute opacity-50 top-[30%] 
-          left-[10%] lg:left-[60%] 
-      text-h3 hidden sm:block lg:block
+          className=" text-[--ourly-theme] absolute opacity-50 
+          top-[30%] left-[10%] 
+          lg:left-[60%] 
+      text-h3 
+      hidden md:hidden lg:block
         font-semibold"
         >
           コミュニケーションを常にとって欲しい…
         </span>
         あなたも会社のこと悩んでいますか？
         <span
-          className=" text-[--ourly-theme] absolute opacity-50 top-[64%] right-[4%] 
-     text-h3 hidden sm:block lg:block
+          className=" text-[--ourly-theme] absolute opacity-50 
+          top-[64%] right-[4%] 
+     text-h3 
+     hidden md:hidden lg:block
         font-semibold"
         >
           皆んなの悩みを解決したい…
         </span>
         <span
-          className=" text-[--ourly-theme] absolute opacity-50 top-[80%] right-[24%] 
-        text-h3 hidden sm:block lg:block
+          className=" text-[--ourly-theme] absolute opacity-50 
+          top-[80%] right-[24%]
+        text-h3 
+        hidden md:hidden lg:block
         font-semibold"
         >
           会社をもっといい職場をさせたい…
