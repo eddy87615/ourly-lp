162e77f74266c58fc2ba97b5275bc2fbec522aec
Eddy Chen <eddychen615@gmail.com>
2023-11-26 21:55:15 +0900

    head added,viewport added.

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -16,6 +16,12 @@ import Qiestion from '@/Question/Question';
 export default function App() {
   return (
     <>
+      <head>
+        <meta
+          name="viewport"
+          content="width=device-width, initial-scale=1,shrink-to-fit=no"
+        />
+      </head>
       <Header />
       <Guide />
       <Slider />
