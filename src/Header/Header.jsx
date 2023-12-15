c20fc725d2cad05f82feb7b0b00d62be8725e0a7
Eddy Chen <eddychen615@gmail.com>
2023-12-15 14:09:53 +0900

    header glass css.

--- src/Header/Header.jsx
+++ src/Header/Header.jsx
@@ -70,7 +70,7 @@ export default function Header() {
     <div
       className={`fixed top-0 left-0 z-[60] flex items-center justify-between
       px-4 lg:px-16 duration-500
-    w-screen h-[100px] ${renderbtn ? '' : 'header'}`}
+    w-screen h-[100px] ${renderbtn ? '' : showBtn ? 'header' : ''}`}
     >
       <Image
         width={300}
